3554a8b119ac11256653d0284086a7d517c05996
James Riley <35643@cgs.act.edu.au>
2015-07-30 15:13:12 +1000

    Merged Feature-Q

---
+++ TestProject/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  TestProject
+//
+//  Created by CGS Spare on 30/07/2015.
+//  Copyright (c) 2015 James Riley. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        println("viewDidLoad")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
